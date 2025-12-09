5975cd4368199af1c8ba43c1711549e8d070c196
arsalanb101 <144160780+arsalanb101@users.noreply.github.com>
2025-12-08 17:20:52 -0700

    Fixed quoting

--- src/axios/login-register-auth-service.js
+++ src/axios/login-register-auth-service.js
@@ -3,6 +3,6 @@ import axios from "axios";
 
 //this instance created juste for only the login and register apis
 const instance=axios.create({
-    baseURL: process.env.REACT_APP_AUTH_SERVICE || https://driving-school-website-2q9l.onrender.com/});
+    baseURL: process.env.REACT_APP_AUTH_SERVICE || "https://driving-school-website-2q9l.onrender.com/"});
 
 export default instance;
